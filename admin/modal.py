277f6d34ea230fedee0edacba8c937721fd9050f
tem12qaz <tem12qaz@yandex.ru>
2022-04-16 21:35:33 +0300

    fix

--- admin/modal.py
+++ admin/modal.py
@@ -5,8 +5,10 @@ body {font-family: Arial, Helvetica, sans-serif;}
 .modal_chat_read {
   display: none; /* Hidden by default */
   position: fixed; /* Stay in place */
-  padding-top: 100px; /* Location of the box */
   z-index: 1; /* Sit on top */
+  padding-top: 100px; /* Location of the box */
+  left: 0;
+  top: 0;
   width: 100%; /* Full width */
   height: 100%; /* Full height */
   overflow: auto; /* Enable scroll if needed */
