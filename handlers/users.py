f39431d70c59adf731526a99b0d6c7bb88a8456f
tem12qaz <tem12qaz@yandex.ru>
2022-05-12 21:02:58 +0300

    init

--- handlers/users.py
+++ handlers/users.py
@@ -147,7 +147,7 @@ async def main_menu(callback: types.CallbackQuery, callback_data):
     elif 'open_' in select:
         table = select.replace('open_', '')
         if tables_order.index(user.max_field) < tables_order.index(table):
-            await callback.answer('not_allowed', show_alert=True)
+            await callback.answer(await get_message('not_allowed'), show_alert=True)
             await callback.message.delete()
         else:
             await callback.answer()
