b6df66eab0b8149d7e89add75365afdd7ad373be
tem12qaz <tem12qaz@yandex.ru>
2022-05-14 12:34:24 +0300

    init

--- admin/views.py
+++ admin/views.py
@@ -86,10 +86,10 @@ class PriorityView(AdminMixin, ModelView):
 class TablePriceView(AdminMixin, ModelView):
     column_list = ('id', 'start', 'start_mentor', 'wood', 'wood_mentor', 'bronze', 'bronze_mentor', 'silver',
                    'silver_mentor', 'gold', 'gold_mentor', 'platinum', 'platinum_mentor',
-                   'legendary' 'legendary_mentor')
+                   'legendary', 'legendary_mentor')
     form_columns = ('start', 'start_mentor', 'wood', 'wood_mentor', 'bronze', 'bronze_mentor', 'silver',
                     'silver_mentor', 'gold', 'gold_mentor', 'platinum', 'platinum_mentor',
-                    'legendary' 'legendary_mentor')
+                    'legendary', 'legendary_mentor')
 
 
 class ConfigView(AdminMixin, ModelView):
