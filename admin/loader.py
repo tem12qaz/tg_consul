9512139641823fa9d28b01e8e381c01298210807
tem12qaz <tem12qaz@yandex.ru>
2022-05-25 17:46:28 +0300

    init

--- admin/loader.py
+++ admin/loader.py
@@ -1,7 +1,7 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-from data.config import BOT_TOKEN
+from config import BOT_TOKEN
 
 bot = Bot(token=BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
