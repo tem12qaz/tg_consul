0396eb284923e633cfd42ed73984c295da071932
tem12qaz <tem12qaz@yandex.ru>
2022-06-01 01:59:20 +0300

    fix

--- admin/bot_app.py
+++ admin/bot_app.py
@@ -30,4 +30,4 @@ async def main_menu(callback: types.CallbackQuery, callback_data):
 if __name__ == '__main__':
     parser = Parser()
     parser.start_parse()
-    executor.start_polling(dp)
+    executor.start_polling(dp, loop=parser.loop)
